cc565cbeacd28582faab09105b167b84db3d507f
Yurii <yuskornyakov@gmail.com>
2015-06-04 10:10:26 +0300

    second try

--- script.js
+++ script.js
@@ -23,114 +23,150 @@ window.onload = function (e) {
         }
     };
 
-    buttonsWrapper.addEventListener('click', function (e) {
-        var classNameToAdd,
-            classNameToRemove,
+    function addClass() {
+        var classNameToAdd = prompt('Class name to add');
+
+        /*if (classNameToAdd !== null && classNameToAdd !== '' &&
+            classNameToAdd.trim().length !== 0) {
+            if (container.className == '') {
+                container.className = classNameToAdd.trim();
+            } else {
+                container.className += (' ' + classNameToAdd.trim());
+            }
+        }*/
+
+        if (classNameToAdd != '' && classNameToAdd != null) {
+            container.classList.add(classNameToAdd);
+        }
+    }
+
+    function removeClass() {
+        var classNameToRemove = prompt('Class name to remove'),
             classListArray,
             classListArrayEdited = [],
-            userInputText,
-            userInputElement,
+            i;
+
+        /*classListArray = container.className.split(' ');
+        for (i = 0; i < classListArray.length; i++) {
+            if (classListArray[i] != classNameToRemove) {
+                classListArrayEdited.push(classListArray[i]);
+            }
+        }
+        container.className = classListArrayEdited.join(' ');
+        if (container.className == '') {
+            container.removeAttribute('class');
+        }*/
+
+        if (classNameToRemove != '') {
+            container.classList.remove(classNameToRemove);
+        }
+    }
+
+    function addTextNode() {
+        var userInputText = prompt('Enter text:');
+
+        if (userInputText != null) {
+            //container.innerText += (' ' + userInputText);
+            container.appendChild(document.createTextNode(userInputText + ' '));
+        }
+    }
+
+    function emptyContainer() {
+        container.innerText = '';
+    }
+
+    function addElement() {
+        var userInputElement,
             userInputElementId,
             userInputElementClass,
-            elem,
-            elementToRemove,
-            targetForm,
+            elem;
+
+        userInputElement = prompt('Element name to add:');
+        userInputElementId = prompt('ID');
+        userInputElementClass = prompt('Class');
+
+        elem = document.createElement(userInputElement);
+        container.appendChild(elem);
+        if (userInputElementId !== '') {
+            elem.id = userInputElementId;
+        }
+        if (userInputElementClass !== '') {
+            elem.className = userInputElementClass;
+        }
+    }
+
+    function removeElement() {
+        var elementToRemove = prompt('Selector to remove:');
+
+        if (container.querySelector(elementToRemove) !== null) {
+            container.removeChild(container.querySelector(elementToRemove));
+        }
+    }
+
+    function serializeAndShow() {
+        var targetForm,
             data = {},
             propName,
             table,
             tbody,
             tr,
             td,
-            i,
             j;
 
+        targetForm = document.querySelector(prompt('Enter forms selector:'));
+
+        if (document.getElementById('table-1') !== null) {
+            body.removeChild(document.getElementById('table-1'));
+        }
+
+        table = body.appendChild(document.createElement('table'));
+        table.id = 'table-1';
+        tbody = table.appendChild(document.createElement('tbody'));
+        formInputList = targetForm.elements;
+
+
+        for (j = 0; j < formInputList.length; j++) {
+            if (formInputList[j].id != 'submit') {
+                data[formInputList[j].id] = formInputList[j].value;
+            }
+        }
+
+        for (propName in data) {
+            if (data.hasOwnProperty(propName)) {
+                tr = tbody.appendChild(document.createElement('tr'));
+                td = tr.appendChild(document.createElement('td'));
+                td.appendChild(document.createTextNode(propName));
+                td = tr.appendChild(document.createElement('td'));
+                td.appendChild(document.createTextNode(data[propName]));
+            }
+        }
+        console.log(data);
+    }
+
+    buttonsWrapper.addEventListener('click', function (e) {
         e.preventDefault();
 
-        if (e.target.value != undefined) {
+        if (e.target.value) {
             if (e.target.value == 'addClass') {
-                classNameToAdd = prompt('Class name to add');
-                if (classNameToAdd !== null && classNameToAdd !== '' &&
-                    classNameToAdd.trim().length !== 0) {
-                    if (container.className == '') {
-                        container.className = classNameToAdd.trim();
-                    } else {
-                        container.className += (' ' + classNameToAdd.trim());
-                    }
-                }
-                //container.classList.add(classNameToAdd);
+                addClass();
             }
             if (e.target.value == 'removeClass') {
-                classNameToRemove = prompt('Class name to remove');
-                classListArray = container.className.split(' ');
-                for (i = 0; i < classListArray.length; i++) {
-                    if (classListArray[i] != classNameToRemove) {
-                        classListArrayEdited.push(classListArray[i]);
-                    }
-                }
-                container.className = classListArrayEdited.join(' ');
-                if (container.className == '') {
-                    container.removeAttribute('class');
-                }
-                //container.classList.remove(classNameToRemove);
+                removeClass();
             }
             if (e.target.value == 'addTextNode') {
-                userInputText = prompt('Enter text:');
-                if (userInputText != null) {
-                    container.innerText += (' ' + userInputText);
-                }
+                addTextNode();
             }
             if (e.target.value == 'emptyContainer') {
-                container.innerText = '';
+                emptyContainer();
             }
             if (e.target.value == 'addElement') {
-                userInputElement = prompt('Element name to add:');
-                userInputElementId = prompt('ID');
-                userInputElementClass = prompt('Class');
-
-                elem = document.createElement(userInputElement);
-                container.appendChild(elem);
-                if (userInputElementId !== '') {
-                    elem.id = userInputElementId;
-                }
-                if (userInputElementClass !== '') {
-                    elem.className = userInputElementClass;
-                }
+                addElement();
             }
             if (e.target.value == 'removeElement') {
-                elementToRemove = prompt('Selector to remove:');
-                if (container.querySelector(elementToRemove) !== null) {
-                    container.removeChild(container.querySelector(elementToRemove));
-                }
+                removeElement();
             }
             if (e.target.value == 'serializeAndShow') {
-                targetForm = document.querySelector(prompt('Enter forms selector:'));
-
-                if (document.getElementById('table-1') !== null) {
-                    body.removeChild(document.getElementById('table-1'));
-                }
-
-                table = body.appendChild(document.createElement('table'));
-                table.id = 'table-1';
-                tbody = table.appendChild(document.createElement('tbody'));
-                formInputList = targetForm.elements;
-
-
-                for (j = 0; j < formInputList.length; j++) {
-                    if (formInputList[j].id != 'submit') {
-                        data[formInputList[j].id] = formInputList[j].value;
-                    }
-                }
-
-                for (propName in data) {
-                    if (data.hasOwnProperty(propName)) {
-                        tr = tbody.appendChild(document.createElement('tr'));
-                        td = tr.appendChild(document.createElement('td'));
-                        td.appendChild(document.createTextNode(propName));
-                        td = tr.appendChild(document.createElement('td'));
-                        td.appendChild(document.createTextNode(data[propName]));
-                    }
-                }
-                console.log(data);
+                serializeAndShow();
             }
         }
     });
